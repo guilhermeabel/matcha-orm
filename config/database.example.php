0b364181e2d94ef262c50d4a10f3d64879a485b9
Guilherme Abel <guilhermeabel8@gmail.com>
2023-04-17 23:51:06 -0300

    add: database example config

---
+++ config/database.example.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'default' => 'mysql',
+    'connections' => [
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'database' => 'your_database',
+            'username' => 'your_username',
+            'password' => 'your_password',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+        ],
+    ],
+];
