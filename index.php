d2c6b844265fad9d5a4b5f8b93189956b4216e63
Guilherme Abel <guilhermeabel8@gmail.com>
2023-04-18 22:39:28 -0300

    add: more examples index

--- index.php
+++ index.php
@@ -10,6 +10,23 @@ $dbConfig = config('connections.' . $defaultConnection);
 $connectionInstance = MatchaORM\Connection::getInstance($dbConfig);
 $pdoConnection = $connectionInstance->getConnection();
 
+// create a user
 $user = new User();
+$user->name = 'John Doe';
+$user->email = 'johndoe@example.com' . rand(1, 100);
+$user->save();
+
+// update a user
+$user = User::find(1);
+$user->name = 'John Doe';
+$user->email = 'johndoe@example.com' . rand(1, 100);
+$user->password = '@%FX' . rand(1, 100);
+$user->save();
+
+// delete a user
+$user = User::find(1);
+$user->delete();
+
+
 echo "<pre>";
 var_dump($user);
