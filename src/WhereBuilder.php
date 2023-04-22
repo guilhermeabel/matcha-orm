119c3419ed03482878224e0cf8386d8e78c58e08
Guilherme Abel <guilhermeabel8@gmail.com>
2023-04-21 23:44:42 -0300

    add: methods for chaining condition

--- src/WhereBuilder.php
+++ src/WhereBuilder.php
@@ -13,4 +13,29 @@ class WhereBuilder
         $this->queryBuilder = $queryBuilder;
     }
 
+    public function and(string $column = null): self
+    {
+        if ($column !== null) {
+            $this->currentColumn = $column;
+        }
+        $this->nextConditionType = 'AND';
+        return $this;
+    }
+
+    public function or(string $column = null): self
+    {
+        if ($column !== null) {
+            $this->currentColumn = $column;
+        }
+        $this->nextConditionType = 'OR';
+        return $this;
+    }
+
+    protected function add(string $conditionType, string $column, string $operator, $value): self
+    {
+        $this->queryBuilder->_addWhere($conditionType, $column, $operator, $value);
+        $this->nextConditionType = 'AND';
+
+        return $this;
+    }
 }
